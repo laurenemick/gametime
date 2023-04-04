a1b0a3f5165f8379fb807cb005fc5af737dd8376
Lauren Emick <laurenemick@Laurens-MacBook-Pro.local>
2023-04-04 15:41:40 -0700

    routing

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Item = ({image, title, subtitle }) => {
+const Item = ({item, image, title, subtitle }) => {
 
     return (
-      <div className="item-container">
-        <img className="item-image" src={image} alt="event" />
-        <div className="item-description">
-          <p className="item-title">{title}</p>
-          <p className="item-subtitle">{subtitle}</p>
+      <Link to={`/item/${item.id}`} state={{item: item}}>
+        <div className="item-container" key={item.id}>
+          <img className="item-image" src={image} alt="event" />
+          <div className="item-description">
+            <p className="item-title">{title}</p>
+            <p className="item-subtitle">{subtitle}</p>
+          </div>
         </div>
-      </div>
+      </Link>
     )
 };
 
--- src/components/Items.js
+++ src/components/Items.js
@@ -9,11 +9,11 @@ const Items = props => {
   );
   return (
     <div style={{visibility: showItems ? 'visible' : 'hidden' }} className="items-container">
-      {events.slice(0, 3).map((event, i) => <Item key={i} image={event.event.map_url} title={event.event.name} subtitle={event.venue.name} />)}
+      {events.slice(0, 3).map((event) => <Item item={event.event} image={event.event.map_url} title={event.event.name} subtitle={event.venue.name} />)}
 
-      {performers.slice(0, 3).map((performer, i) => <Item key={i} image={performer.hero_image_url} title={performer.name} subtitle={performer.category} />)}
+      {performers.slice(0, 3).map((performer) => <Item item={performer} image={performer.hero_image_url} title={performer.name} subtitle={performer.category} />)}
 
-      {venues.slice(0, 3).map((venue, i) => <Item key={i} image={venue.image_url} title={venue.name} subtitle={venue.city} />)}
+      {venues.slice(0, 3).map((venue) => <Item item={venue} image={venue.image_url} title={venue.name} subtitle={venue.city} />)}
     </div>
   )
 };
